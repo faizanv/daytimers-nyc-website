835494fed8475757d3652780eab429d3229f03e8
faizanv <faizan.virani.44@gmail.com>
2026-08-17 23:54:22 -0400

    hopefully fix deploys and remove wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-    imageService: 'cloudflare'
-  })
+  output: "static",
 });
